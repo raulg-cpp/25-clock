6f4a8bf1348b7bd9fb665d6f9ceeb2fa0d71c76a
raulg-cpp <raulg.cpp@gmail.com>
2023-06-01 19:36:15 -0400

    crude timer function

--- references.md
+++ references.md
@@ -2,3 +2,41 @@
 
 ## Timer functionality
 https://pomodorotimer.online/
+
+## CSS
+https://www.davekb.com/browse_programming_tips:html_unicode_play_stop_symbols:txt
+
+https://www.w3schools.com/colors/colors_picker.asp
+
+## Javascript
+https://dirask.com/posts/JavaScript-measure-elapsed-time-between-setInterval-intervals-1yKGNp
+
+https://www.w3schools.com/js/js_timing.asp
+
+## Timers
+
+__Generate from scratch__:
+
+https://www.scaler.com/topics/react/react-timer/
+
+https://dev.to/yuridevat/how-to-create-a-timer-with-react-7b9
+
+https://medium.com/bb-tutorials-and-thoughts/how-to-create-a-countdown-timer-in-react-app-e99916046292
+
+https://www.digitalocean.com/community/tutorials/react-countdown-timer-react-hooks
+
+https://blog.greenroots.info/how-to-create-a-countdown-timer-using-react-hooks
+
+https://stackblitz.com/edit/react-hook-countdown?file=src%2FApp.js
+
+https://www.geeksforgeeks.org/how-to-create-a-countdown-timer-using-reactjs/
+
+https://blog.greenroots.info/how-to-create-a-countdown-timer-using-react-hooks
+
+__React component__:
+
+https://www.npmjs.com/package/react-countdown
+
+https://github.com/ndresx/react-countdown/blob/master/examples/src/CountdownApi.tsx
+
+https://codesandbox.io/s/elastic-euclid-6vnlw
--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
+/* References:
+	https://www.w3resource.com/javascript-exercises/javascript-basic-exercise-51.php
+*/
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+	//=== State ===
+	const [totalTime, setTotalTime] = useState( 25 * 60e3 );
+	const [initTime, setInitTime] = useState( 25 * 60e3 );
+
+	//=== Functions ===
+	function timeConvert(num) { 
+		num = num / 1000;
+  		var hours = Math.floor(num / 60);  
+  		var minutes = Math.floor(num % 60);
+  		return hours + ":" + minutes;         
+	}
+		
+	const updateTimer = () => {
+		var change_time = initTime - performance.now();
+		document.getElementById("currTime").innerHTML = timeConvert(change_time); 
+	}
+		
+	//updateTimer();
+	const funcPlay = () => {
+		setInitTime( totalTime + performance.now() );
+		setInterval( updateTimer, 1000 );
+	}
+
+	//=== JSX === 
   return (
     <div className="App">
     	<h1 className="title">25 + 5 Clock</h1>
     	<div className="mainBox">
+    		{/* Input buttons */}
     		<div className="input">
     			<div className="timer">
     				<h2>Break length</h2>
@@ -14,7 +44,6 @@ function App() {
     					<button>&#9660;</button>
     				</div>
     			</div>
-    			
     			<div className="timer">
     				<h2>Session length</h2>
     				<span>timer</span>
@@ -26,13 +55,13 @@ function App() {
     		</div>
     		<div className="output">
     			<h2>Session</h2>
-    			<h1>timer</h1>
+    			<h1 id="currTime">25:0</h1>
     		</div>
-    		
+    		{/* Output display */}
     		<div className="outputButtons">
-    			<button>&#9658;</button> 
-    			<button>&#9724;</button> 
-    			<button>&#8635; </button> 
+    			<button id="play" onClick={funcPlay}> &#9658; </button> 
+    			<button id="pause"> &#9724; </button> 
+    			<button id="reset"> &#8635; </button> 
     		</div>
     	</div>
     </div>
